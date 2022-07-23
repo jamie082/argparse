f48740eec89bcbc418021fb7f3980b18cb8e3122
Jamie Morrissey <jamiemorrissey@gmail.com>
2022-07-23 01:30:56 -0700

    added files to repo

---
+++ prog_2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import argparse
+
+parser = argparse.ArgumentParser()
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-i", "--input", action="store_true")
+group.add_argument("-u", "--user", action="store_true")
+group.add_argument("-o", "--output", action="store_true")
+parser.add_argument("x", type=int, help="the base")
+parser.add_argument("y", type=int, help="the exponent")
+args = parser.parse_args()
+answer = args.x**args.y
+
+if args.mode:
+    print(answer)
+elif args.verbose:
+    print(f"{args.x} to the power {args.y} equals {answer}")
+else:
+    print(f"{args.x}^{args.y} == {answer}")
