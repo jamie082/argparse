e691a36702813f0efb40ba782d23c313f28836a6
Jamie Morrissey <jamiemorrissey@gmail.com>
2022-07-23 22:30:52 -0700

    fixed source

--- prog_2.py
+++ prog_2.py
@@ -2,8 +2,12 @@
 
 import argparse
 
+# http://docs.python.org/3/howto/argparse.html
+
 parser = argparse.ArgumentParser()
 group = parser.add_mutually_exclusive_group()
+group.add_argument("-v", "--verbose", action="store_true")
+group.add_argument("-q", "--quiet", action="store_true")
 group.add_argument("-i", "--input", action="store_true")
 group.add_argument("-u", "--user", action="store_true")
 group.add_argument("-o", "--output", action="store_true")
@@ -16,5 +20,14 @@ if args.quiet:
     print(answer)
 elif args.verbose:
     print(f"{args.x} to the power {args.y} equals {answer}")
+elif args.input:
+    print(f"{args.input}")
+elif args.output:
+    print(f"{args.output}")
+elif args.user:
+    print(f"{args.user}")
+elif args.output:
+    print(f"{args.output}")
+
 else:
     print(f"{args.x}^{args.y} == {answer}")
