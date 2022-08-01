9a6b6aa41a1aa41bda5d770f5e26d8f4769c4c82
Jamie Morrissey <jamiemorrissey@gmail.com>
2022-08-01 02:01:19 -0700

    added files

--- prog_2.py
+++ prog_2.py
@@ -5,13 +5,14 @@ import argparse
 # http://docs.python.org/3/howto/argparse.html
 
 parser = argparse.ArgumentParser()
-group = parser.add_mutually_exclusive_group()
-group.add_argument("-v", "--verbose", action="store_true")
-group.add_argument("-q", "--quiet", action="store_true")
-group.add_argument("-i", "--input", help="input sample", action="store_true")
-group.add_argument("-u", "--user", help="user sample", action="store_true")
-group.add_argument("-o", "--output", help="output sample", action="store_true")
-group.add_argument('--values', type=int, nargs=3)
+#group = parser.add_mutually_exclusive_group()
+parser.add_argument("-v", "--verbose", action="store_true")
+parser.add_argument("-q", "--quiet", action="store_true")
+parser.add_argument("-i", "--input", help="input sample", action="store_true")
+parser.add_argument("-u", "--user", help="user sample", action="store_true")
+parser.add_argument("-o", "--output", help="output sample", action="store_true")
+parser.add_argument('--values', type=int, nargs=3)
+parser.add_argument('--name', type=str, required=True)
 args = parser.parse_args() # blah
 
 
@@ -20,11 +21,10 @@ if args.input: # -i was typed
 elif args.user: # -u was typed
     print(f"Your user file is: {args.user}")
 elif args.output: # -o was typed
-    print(f"Your user file is: {args.output}")
+    print(f"Your user file is: {args.output}") 
 
-else:
-    print("Error! Please try Again")
+elif args.name:
+    print('Hello,', args.name)
 
 sum = sum(args.values)
 print('sum:', sum)
-
