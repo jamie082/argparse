5fcb606df08ef743bee1bd4cae33f05b7d5f0caf
Jamie Morrissey <jamiemorrissey@gmail.com>
2022-08-01 13:13:35 -0700

    added positional argument

--- prog_2.py
+++ prog_2.py
@@ -5,12 +5,12 @@ import argparse
 # http://docs.python.org/3/howto/argparse.html
 
 parser = argparse.ArgumentParser()
-#group = parser.add_mutually_exclusive_group()
-parser.add_argument("-v", "--verbose", action="store_true")
-parser.add_argument("-q", "--quiet", action="store_true")
-parser.add_argument("-i", "--input", help="input sample", action="store_true")
-parser.add_argument("-u", "--user", help="user sample", action="store_true")
-parser.add_argument("-o", "--output", help="output sample", action="store_true")
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-i", "--input", help="input sample", action="store_true")
+group.add_argument("-u", "--user", help="user sample", action="store_true")
+group.add_argument("-o", "--output", help="output sample", action="store_true")
+parser.add_argument('x', type=int) # positional argument x 
+parser.add_argument('y', type=int) # positional argument y
 parser.add_argument('--values', type=int, nargs=3)
 parser.add_argument('--name', type=str, required=True)
 args = parser.parse_args() # blah
@@ -26,5 +26,7 @@ elif args.output: # -o was typed
 elif args.name:
     print('Hello,', args.name)
 
+'''
 sum = sum(args.values)
 print('sum:', sum)
+'''
