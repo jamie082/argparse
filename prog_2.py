28cd951785ae15f43ce79637c69c1442cc2a3b31
Jamie Morrissey <jamiemorrissey@gmail.com>
2022-08-05 10:44:47 -0700

    finished program

--- prog_2.py
+++ prog_2.py
@@ -6,13 +6,11 @@ import argparse
 
 parser = argparse.ArgumentParser()
 group = parser.add_mutually_exclusive_group()
-group.add_argument('--values', type=int, nargs=3)
-parser.add_argument('--name', type=str, required=True)
 group.add_argument('--add', action='store_true')
 group.add_argument('--subtract', action='store_true')
 parser.add_argument('x', type=int, help="positional argument x") # positional argument x
 parser.add_argument('y', type=int, help="positional argument y") # positional argument y
-args = parser.parse_args() # blah
+args = parser.parse_args()
 
 if args.add:
     total = args.x + args.y
@@ -22,10 +20,3 @@ elif args.subtract:
     difference = args.x - args.y
     print ('Difference:', difference)
 
-elif args.name:
-    print('Hello,', args.name)
-
-'''
-sum = sum(args.values)
-print('sum:', sum)
-'''
